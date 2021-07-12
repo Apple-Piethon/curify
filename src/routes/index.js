50a5595f8eceb43d28e3e38607beff90242149fc
britneywwc <britneywwc@gmail.com>
2021-07-13 01:17:22 +0800

    Changed index and login capitalization

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
-import Index from '../pages/index/index'
+import Index from '../pages/Index/'
 import NotFound from '../pages/NotFound'
-import Login from '../pages/login'
+import Login from '../pages/Login'
 
 const Routes = () => {
     return (
