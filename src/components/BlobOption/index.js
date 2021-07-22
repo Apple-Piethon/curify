df070aebfee65c58d12583e421da9dbd35fecd25
britneywwc <britneywwc@gmail.com>
2021-07-22 19:16:39 +0800

    Added button components for pick show page

---
+++ src/components/BlobOption/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Box } from '@material-ui/core';
+import { ReactComponent as MovieBlob } from '../../assets/images/ps-blob1.svg';
+import { ReactComponent as SeriesBlob } from '../../assets/images/ps-blob2.svg';
+import { ReactComponent as BothBlob } from '../../assets/images/ps-blob3.svg';
+
+const BlobOption = props => {
+    const option = props.option;
+
+    return (
+        <Box display="flex" justifyContent="center" alignItems="center">
+            <div className="ps-blob">
+                {option === "Movies" ? <MovieBlob/>
+                : option === "Series" ? <SeriesBlob/>
+                : <BothBlob />}
+            </div>
+            <div className="ps-blob-title">
+                {option}
+            </div>
+        </Box>
+    )
+}
+
+export default BlobOption
--- src/pages/PickShow/index.js
+++ src/pages/PickShow/index.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import { Box, Grid } from '@material-ui/core';
-import { View, Text } from 'react-native';
+import { Box, Grid, SvgIcon, DeleteIcon} from '@material-ui/core';
+import { ReactComponent as TitleBlob } from '../../assets/images/ps-title.svg';
+import BlobOption from '../../components/BlobOption';
 
 const PickShow = () => {
     return (
-        <Box
-            display="grid"
+        <Box 
+            display="grid" 
             bgcolor="#EEEEE"
-            alignItems="center"
-            justifyContent="center"
-        >
+            alignItems="center" 
+            justifyContent="center">
 
-        <View>
-            <svg width="1284" height="353" viewBox="0 0 1284 353" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1284 293.265C1284 423.143 1042.5 299.265 724 293.265C305.092 293.265 0 361.415 0 77.2646C0 -100.235 288.5 87.2645 724 77.2646C1170.5 67.012 1284 45.5298 1284 293.265Z" fill="#B6A4A4" fill-opacity="0.2"/>
-            </svg>
-            <Text>
+            <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <div className="ps-title-box">
+                    <TitleBlob/>
+                </div>
                 <div className="ps-title">
                     What are you in the mood for?
                 </div>
-            </Text>
-        </View>
+            </Box>
+
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+            >
+
+               <BlobOption option="Movies"/>
+
+            </Grid>
+
         </Box>
     )
 }
